56be648420191cd31d0f627657b4cb20e3d92695
Michael Lamb <503lamb@gmail.com>
2022-04-22 09:28:57 -0600

    inline posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'main#index'
 
-  resources :discussions, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :discussions, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
+    resources :posts, only: [:create], module: :discussions
+  end
 end
